63853f602e34caffbd3f6f39c6f8077e786c73b7
Maxim Kolganov <kolganov.mv@gmail.com>
2012-09-26 09:20:03 +0400

    tree rotations

--- ITMOPrelude/Tree.hs
+++ ITMOPrelude/Tree.hs
@@ -18,7 +18,11 @@ addToLeftLeaf x Leaf = Node x Leaf Leaf
 addToRightLeaf x (Node a l r) = Node a l (addToRightLeaf x r)
 addToRightLeaf x Leaf = Node x Leaf Leaf
 
--- rotations? which rotations
+leftRotate (Node x l (Node y l' r')) = Node y (Node x l l') r'
+leftRotate n = error "leftRotate: Incorrect tree"
+
+rightRotate (Node x (Node y l' r') r) = Node y l' (Node x r' r)
+rightRotate n = error "rightRotate: Incorrect tree"
 
 tmap :: (a -> b) -> Tree a -> Tree b
 tmap f Leaf = Leaf
@@ -27,3 +31,4 @@ tmap f (Node x l r) = Node (f x) (tmap f l) (tmap f r)
 tfold :: (a -> b -> b) -> b -> Tree a -> b
 tfold f z Leaf = z
 tfold f z (Node x l r) = f x (tfold f (tfold f z r) l)
+
