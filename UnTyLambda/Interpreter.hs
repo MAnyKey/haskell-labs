dc686f54d5cb4f8399653cbfdb5db3d2a9a8123a
Maxim Kolganov <kolganov.mv@gmail.com>
2012-10-04 22:01:42 +0400

    pull from jan repo

--- UnTyLambda/Interpreter.hs
+++ UnTyLambda/Interpreter.hs
@@ -1,35 +1,46 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+--------------------------------------------------------------------------------
+-- В данном задании требуется реализовать интерпретатор для
+-- нетипизированной лямбды
+--------------------------------------------------------------------------------
+
 module UnTyLambda.Interpreter where
 
+-- Какие-то импорты. Заметьте, что в этом задании надо
+-- использовать обычную Prelude
+import Prelude hiding (catch)
+import Control.Exception
+
+------------------------------------------------------------
+-- Определение дататайпа для нетипизированной лямбды
 type Variable = String
+data Term = Var Variable | Lam Variable Term | App Term Term deriving (Show,Read)
 
--- Лямбда-терм
-data Term = Var Variable
-          | Abs Variable Term
-          | App Term Term
-          deriving (Show)
+------------------------------------------------------------
+-- Дальше всё на ваше усмотрение
 
--- Тип [ a ] == Типу List a
--- значение [] == значению Nil
--- значение [ a ] == значению Cons a Nil
--- конструктор (:) == конструктору Cons
+-- Если внутри будете использовать именованное представление, то
+-- я тут решил немного вам помочь
+-- (иначе говоря, код из этого раздела можно совсем выкинуть,
+-- если хочется)
 
--- Свободные переменные терма
-free (Var v) = [v]
-free (Abs v t) = filter (/= v) . free $ t -- /= это <<не равно>>
+free (Var v)    = [ v ]
+free (Lam v t)  = filter (/= v) . free $ t
 free (App t t') = (free t) ++ (free t')
 
-binded (Var v) = []
-binded (Abs v t) = v : binded t
-binded (App t t') = binded t ++ binded t'
+-- subst :: Term -> Variable -> Term -> Term
+-- subst t@(Var v)   var what = if v == var then what else t
+-- subst t@(Lam v b) var what = if v == var then t else Lam v (subst b var what)
+-- subst (App t t')  var what = App (subst t var what) (subst t' var what)
 
--- Заменить все свободные вхождения переменной var на what в терме term
 subst var what term = case term of
     Var v    -> if v == var then what else term
-    Abs v t  -> if v == var then term else Abs v (subst var what t)
+    Lam v t  -> if v == var then term else Lam v (subst var what t)
     App t t' -> App (subst var what t) (subst var what t')
 
--- Генерирует список имён, производных от v, не входящих в fv
-newname fv v = head . filter (\x -> not . elem x $ fv) . iterate ('_':) $ v
+newname fv = head . filter (not . flip elem fv) . iterate ('_':)
+
+--- ...
 
 -- Обычная бета-редукция, хендлящая переименования переменных
 betaReduct :: Variable -> Term -> Term -> Term
@@ -37,117 +48,115 @@ betaReduct var what term = subst var what $ renameBindings (free what) term
   where renameBindings vars subterm = case subterm of 
           Var _ -> subterm
           App t t' -> App (renameBindings vars t) (renameBindings vars t')
-          Abs n t -> Abs nn newt
+          Lam n t -> Lam nn newt
             where nameUsed = elem n vars
                   nn = if nameUsed then newname (vars ++ free t) n else n
                   newt = if nameUsed then subst n (Var nn) t else t
 
 hasRedexes (Var _) = False
-hasRedexes (Abs v t) = hasRedexes t
-hasRedexes (App (Abs _ t) t') = True
+hasRedexes (Lam v t) = hasRedexes t
+hasRedexes (App (Lam _ t) t') = True
 hasRedexes (App t t') = hasRedexes t || hasRedexes t'
 
--- Нормализация нормальным порядком терма term
-normal' :: Term -> Term
-normal' term = if (hasRedexes term) then normal' $ normalReduce term else term 
-        
 normalReduce term = case term of
   Var _ -> term
-  Abs var subterm -> Abs var $ normalReduce subterm
-  App (Abs var subterm) term' -> betaReduct var term' subterm
+  Lam var subterm -> Lam var $ normalReduce subterm
+  App (Lam var subterm) term' -> betaReduct var term' subterm
   App term term' -> if hasRedexes term
                     then App (normalReduce term) term'
                     else App term $ normalReduce term'
 
--- Нормализация аппликативным порядком терма term
-applicative' :: Term -> Term
-applicative' term = if (hasRedexes term) then applicative' $ applicativeReduce term else term
-
 applicativeReduce term = case term of
   Var _ -> term
-  Abs var subterm -> Abs var $ applicativeReduce subterm
+  Lam var subterm -> Lam var $ applicativeReduce subterm
   App term term' -> if hasRedexes term' 
                     then App term $ applicativeReduce term' 
                     else case term of
-                      Abs v subt -> betaReduct v term' subt
+                      Lam v subt -> betaReduct v term' subt
                       _ -> App (applicativeReduce term) term'
 
+inWeakHeadForm (Var _) = True
+inWeakHeadForm (Lam _ _) = True
+inWeakHeadForm (App (Lam _ t) t') = False
+inWeakHeadForm (App t t') = inWeakHeadForm t
 
--- Маркер конца ресурсов
-data TooLoong = TooLoong deriving Show
+weakHeadReduce term = case term of
+  App (Lam v t) t2 -> subst v t2 t
+  App t1 t2 | not (inWeakHeadForm t1) -> App (weakHeadReduce t1) t2
+  _ -> term
 
--- (*) Нормализация нормальным порядком терма term за неболее чем n шагов.
--- Результат: Или числа итераций недостаточно, чтобы достичь нормальной
--- формы. Или (число нерастраченных итераций, терм в нормальной форме).
--- 
-normal :: Int -> Term -> Either TooLoong (Int, Term)
-normal n term 
-  | n < 0  = Left TooLoong
+------------------------------------------------------------
+-- За исключением того, что требуется реализовать следующие
+-- стратегии нормализации (они все принимают максимальное
+-- число шагов интерпретатора в качестве первого 
+-- параметра (n); если за n шагов нормализовать не удаётся,
+-- то следует бросать error, тестер его поймает):
+
+wh, no, wa, sa :: Integer -> Term -> Term
+
+-- Редукция аппликативным порядком
+sa n term 
+  | n < 0  = error $ "Too long sequence at [" ++ show term ++ "]"
   | otherwise = if (hasRedexes term) 
-                then normal (n - 1) $ normalReduce term
-                else Right (n, term)
+                then sa (n - 1) $ applicativeReduce term
+                else term
 
--- (*) Аналогичная нормализация аппликативным порядком.
-applicative :: Int -> Term -> Either TooLoong (Int, Term)
-applicative n term 
-  | n < 0  = Left TooLoong
+-- Нормализация нормальным порядком
+no n term 
+  | n < 0  = error $ "Too long sequence at [" ++ show term ++ "]"
   | otherwise = if (hasRedexes term) 
-                then applicative (n - 1) $ applicativeReduce term
-                else Right (n, term)
+                then no (n - 1) $ normalReduce term
+                else term
 
--- (***) Придумайте и реализуйте обобщённую функцию, выражающую некоторое
--- семейство стратегий редуцирования. В том смысле, что номальная, нормальная
--- головная, нормальная слабо-головная и аппликативная стратегии
--- при помощи этой функции будут выражаться некоторым элементарным образом.
--- Аргумент n можно отбросить, а можно оставить.
---
--- strategy = ?
---
--- normal = strategy ?
--- hnf = strategy ?
--- whnf = strategy ?
--- applicative = strategy ?
---
--- Какие ещё стратегии редуцирования вы знаете? Можно ли их выразить
--- при помощи этой стратегии? Если да, то как?
--- Если нет, то можно ли реализовать аналогичную функцию для _всех_
--- возможных стратегий редуцирования, а не только для такого семейства?
--- Если да, то как? Если нет, то почему?
+-- Редукция в слабую головную нормальную форму
+wh n term
+  | n < 0 = error $ "Too long sequence at [" ++ show term ++ "]"
+  | otherwise = if (inWeakHeadForm term) 
+                then term
+                else wh (n - 1) $ weakHeadReduce term
 
---------------------------------------------------------
 
--- Область тестирования
+-- (*) (не обязательно) Редукция "слабым" аппликативным порядком.
+-- Отличается от обычного аппликативного тем, что не лезет внутрь
+-- лямбд и правые части аппликаций, когда это возможно.
+wa = undefined
 
-loop' = Abs "x" $ App (Var "x") (Var "x")
-loop = App loop' loop'
+-- Замечание: cкорость работы вашего интерпретатора специально не оценивается,
+-- потому можно использовать свой изоморфный (с точностью до альфа-конверсии)
+-- тип для представления термов и преобразовывать Term в него и обратно.
 
-u = Abs "a" $ Abs "b" $ App (Var "a") $ App (Var "b") (Var "_b")
-v = Abs "a" $ Abs "b" $ App (App (Var "a") (Var "b")) (Var "_b")
-w = Abs "a" $ Abs "b" $ Abs "c" $ Abs "d" $ App (App (Var "a") (Var "b")) (App (Var "c") (Var "d"))
+-- Перечисление всех этих порядков (в порядке отличном от
+-- определения, да)
+orders =
+    [ ("wh", wh)
+    , ("no", no)
+--    , ("wa", wa) -- Можно раскоментировать, да
+    , ("sa", sa) ]
 
-main = test 100
-    [ ("no", normal)
-    , ("ap", applicative) ]
+------------------------------------------------------------
+-- Игнорируйте это, если выглядит непонятно
+pall term = mapM_ $ \(d, x) -> putStr (d ++ ": ") >> catch (let t = x 1000 term in seq t (print t)) (\(e :: SomeException) -> print e)
+testfuncs funcs = mapM_ $ \t -> putStr "===== " >> print t >> pall t funcs
+
+------------------------------------------------------------
+-- Сюда можно добавлять тесты
+lamxx = Lam "x" $ App (Var "x") (Var "x")
+omega = App lamxx lamxx
+
+test = testfuncs orders
     [ Var "a"
-    , u
-    , v
-    , loop'
-    , u `App` Var "a"
-    , v `App` Var "a"
-    , u `App` Var "b"
-    , v `App` Var "b"
-    , u `App` Var "_b"
-    , v `App` Var "_b"
-    , (u `App` Var "_b") `App` Var "_b"
-    , (v `App` Var "_b") `App` Var "_b"
-    , w
-    , w `App` (Abs "a" (Var "a") `App` (Abs "b" $ Var "b"))
-    , (w `App` Abs "a" (Var "b")) `App` loop
-    , loop
+    , Lam "x" $ (Lam "y" $ Var "y") `App` (Var "x")
+    , (Lam "x" $ Lam "y" $ Var "x") `App` (Var "y")
+    , omega
     ]
 
--- Если вы не понимаете как это работает, то пока и не надо
-pall n term  = mapM_ (\(desc, reduce) -> putStr (desc ++ ": ") >> print (reduce n term))
-
-test :: Show a => Int -> [(String, Int -> Term -> a)] -> [Term] -> IO ()
-test n funcs = mapM_ (\term -> print term >> pall n term funcs)
+------------------------------------------------------------
+-- Немного теоретических замечаний, если они вас волнуют
+--
+-- Следует специально отметить, что поскольку в конце теста
+-- результат вычисления печатают, то ленивость Haskell не
+-- влияет на семантику интерпретируемого исчисления.
+--
+-- Чтобы это особенно подчеркнуть в тестере выше я написал
+-- seq в интересном месте (хотя конкретно это там ничего не
+-- гарантирует, на самом-то деле).
